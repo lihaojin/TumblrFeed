50aae5029984617f64adaf5b1f9a8d6d88f3cf87
lihaojin <hli002@citymail.cuny.edu>
2018-10-06 19:57:12 -0400

    first commit

---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr Feed
+//
+//  Created by Candice Zhong on 10/6/18.
+//  Copyright © 2018 Haojin Li. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
